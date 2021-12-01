49bee046deb9ea09aa597382d52fde8667195ab4
vaibhavnathj-amsbio <vaibhavnathj@amsbio.com>
2021-12-01 11:48:52 +0100

    added stock picking model

--- customTemplate/__manifest__.py
+++ customTemplate/__manifest__.py
@@ -16,7 +16,7 @@
     'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'stock'],
 
     # always loaded
     'data': [
